5cca8931b76ee0feb3303d5643d366c25c216cde
Davide Albano <dalbano@1-C-32.42heilbronn.de>
2024-10-18 16:43:31 +0200

    idk bout bonus

--- srcs/ft_atoi_flags.c
+++ srcs/ft_atoi_flags.c
@@ -6,21 +6,21 @@
 /*   By: dalbano <dalbano@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/18 15:07:25 by dalbano           #+#    #+#             */
-/*   Updated: 2024/10/18 15:15:28 by dalbano          ###   ########.fr       */
+/*   Updated: 2024/10/18 16:42:15 by dalbano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_printf.h"
 
-// int	ft_atoi_flags(const char **str, t_flags *flags)
-// {
-// 	int	value;
+int	ft_atoi_flags(const char **str)
+{
+	int	value;
 
-// 	value = 0;
-// 	while (**str >= '0' && **str <= '9')
-// 	{
-// 		value = (value * 10) + (**str - '0');
-// 		(*str)++;
-// 	}
-// 	return (value);
-// }
+	value = 0;
+	while (**str >= '0' && **str <= '9')
+	{
+		value = (value * 10) + (**str - '0');
+		(*str)++;
+	}
+	return (value);
+}
